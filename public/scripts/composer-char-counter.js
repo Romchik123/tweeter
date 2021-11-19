e8b6a52bace7446c3c3faa5b0c062dcde0061307
rom <mozzieisme@gmail.com>
2021-11-18 20:00:40 -0500

    Add comments to the file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,20 @@
-// eslint-disable-next-line no-undef
-$(document).ready(function () {
-  // --- our code goes here ---
+/* eslint-disable no-undef */
 
-  $("#tweet-text").on("input", function () {
+
+// jQuery ready function ::
+$(document).ready(function() {
+
+  // Tweeter characters counter ::
+  $("#tweet-text").on("input", function() {
     let count = $(this).val().length;
 
+    // Counting backwards ::
     let result = 140 - count;
     $(".counter").text(result);
 
     $(".counter").removeClass("warning");
 
+    // If amount of characters exceeds -1 it will be colored in red ::
     if (result < 0) {
       $(".counter").addClass("warning");
     }
