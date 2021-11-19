1a3c66cb87e826eed77825ac9b238d115aa1ff0f
rom <mozzieisme@gmail.com>
2021-11-18 19:46:36 -0500

    Improve the code and add comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+/* eslint-disable no-undef */
+// Client-side JS logic goes here ::
 
-const escape = function (str) {
+// Function to avoid malicious attacks ::
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -13,36 +11,7 @@ const escape = function (str) {
 
 
 
-
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
-
-
-
-
+// Creating Ajax html tweet element ::
 const createTweetElement = (tweetData) => {
   const tweetCreatedAt = timeago.format(tweetData.created_at);
 
@@ -77,17 +46,13 @@ const createTweetElement = (tweetData) => {
 
 
 
-
-const renderTweets = function (data) {
+// Rendering the data to the page ::
+const renderTweets = function(data) {
   // takes return value and appends it to the tweets container
-
   $("#tweets-container").empty();
 
   for (const iterator of data) {
     const $tweet = createTweetElement(iterator);
-
-    // Test / driver code (temporary)
-
     $("#tweets-container").prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   }
 };
@@ -95,36 +60,40 @@ const renderTweets = function (data) {
 
 
 
+// Implementing the Ajax and jQuery ::
+$(document).ready(function() {
 
-$(document).ready(function () {
-  const loadTweets = function () {
-    $.ajax("/tweets/", { method: "GET" }).then(function (res) {
+  // Ajax get request ::
+  const loadTweets = function() {
+    $.ajax("/tweets/", { method: "GET" }).then(function(res) {
       renderTweets(res);
     });
   };
 
   loadTweets();
 
-  $("#target").submit(function (event) {
+
+  // Ajax, Submitting the post request ::
+  $("#target").submit(function(event) {
     event.preventDefault();
 
     const result = $(this).find("#tweet-text").val();
     const text = $(this).serialize();
 
+    // Conditions for posting the tweets ::
     if (result === "" || result === null) {
       $(".warning-2").hide();
       $(".warning-1").show();
     } else if (result.length > 140) {
       $(".warning-1").hide();
       $(".warning-2").show();
-      console.log("hey");
     } else {
       $("#tweet-text").val("");
       $(".counter").val(140);
 
       $(".warning-1").hide();
       $(".warning-2").hide();
-      $.ajax("/tweets/", { method: "POST", data: text }).then(function (res) {
+      $.ajax("/tweets/", { method: "POST", data: text }).then(function() {
         loadTweets();
       });
     }
